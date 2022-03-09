273796d796ea3d0f77f018c125aeb42651aa2188
Dawid Klimowicz <dawidkl777@gmail.com>
2022-03-09 14:12:43 +0100

    Oferty_deatils wstepny wyglad, zmieniony appbar

--- lib/pages/oferty_deatils_page.dart
+++ lib/pages/oferty_deatils_page.dart
@@ -15,50 +15,80 @@ class OfertyDetailsPage extends StatelessWidget {
       required this.cena});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Colors.yellow[600]!, Colors.yellow[700]!]),
-          ),
-        ),
-        backgroundColor: Colors.yellow[600],
-        elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: IconButton(
-                icon: Icon(
-                  Icons.notifications_outlined,
-                  size: 20,
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: CircleAvatar(
+                backgroundColor: Colors.yellowAccent[700],
+                child: Icon(
+                  Icons.navigate_before,
+                  size: 35,
+                  color: Colors.white,
                 ),
-                color: Colors.white,
-                onPressed: () {},
               ),
             ),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
-                image,
-                fit: BoxFit.fitHeight,
+          body: Column(
+            children: [
+              Stack(
+                children: <Widget>[
+                  Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(20.0),
+                          topRight: const Radius.circular(20.0),
+                        ),
+                        child: Image.asset(
+                          image,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: -970,
+                    child: Container(
+                      alignment: Alignment.topLeft,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white),
+                      width: MediaQuery.of(context).size.width,
+                      height: 1000,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(20, 20, 0, 5),
+                            child: Text("$title",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+                overflow: Overflow.visible,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
